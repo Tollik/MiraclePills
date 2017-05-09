076ff4cc1b211bfc32cdcf2c731b806eb99f17da
Tollik <toliks1121@gmail.com>
2017-05-09 13:08:39 +0300

    Initial Commit

---
+++ MiraclePills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePills
+//
+//  Created by Tolik Ivanov on 09/05/2017.
+//  Copyright © 2017 Tolik Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
